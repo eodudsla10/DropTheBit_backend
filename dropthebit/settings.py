37f8d0fe5eb522b12faf84ba196e4bf77c2bd11c
jeondeayeong <eodudsla10@naver.com>
2021-04-27 02:31:05 +0900

    backend 0.0.3 update

--- dropthebit/settings.py
+++ dropthebit/settings.py
@@ -20,8 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 from decouple import config
-import os
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+# import os
+# SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
